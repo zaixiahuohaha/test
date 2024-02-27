20ff19d97a70b30f2b57f0745c9b2c8761a12adf
czb <2439630833@qq.com>
2024-02-27 17:39:40 +0800

    修改启动文案

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,13 +1,21 @@
 package com.example.demo;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.env.Environment;
 
 @SpringBootApplication
 public class DemoApplication {
 
+	private static final Logger LOG = LoggerFactory.getLogger(DemoApplication.class);
+
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+		SpringApplication app = new SpringApplication(DemoApplication.class);
+		Environment env = app.run(args).getEnvironment();
+		LOG.info("启动成功！！");
+		LOG.info("地址: \thttp://127.0.0.1:{}", env.getProperty("server.port"));
 	}
 
 }
